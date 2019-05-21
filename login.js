fff9a62aaebc09d256666315c0af5887b9b82e3b
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-05-20 19:17:41 -0500

    Added a login mod

--- login.js
+++ login.js
@@ -1,14 +1,8 @@
-const toggleMenu = () => {
-    // Toggle the "menu--open" class on your menu refence. 
-    menu.classList.toggle('menu--open');
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  
-  // Start Here: Create a reference to the ".menu" class
-  const menu;
-  const menu = document.querySelector('.menu');
-  // create a reference to the ".menu-button" class
-  const menuButton;
-  const menuButton = document.querySelector('.menu-button');
-  // Using your menuButton reference, add a click handler that calls toggleMenu
-  
-  menuButton.addEventListener('click', () => toggleMenu());
+}
