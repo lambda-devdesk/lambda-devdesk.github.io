faa80cd30f1fbd5c750577e565961701f27ac080
Spencer Summerville <me@grenuttag.com>
2019-05-21 20:24:42 -0400

    Further development on sign up modal
    - Stylized a nice close button on the right side of the modal.
    - Added a register button to the modal where it reveals hidden form information to create an account.

--- js/login.js
+++ js/login.js
@@ -6,6 +6,7 @@ let hideModal = () => modal.classList.remove("visible");
 let ctaButton = document.querySelector(".cta");
 ctaButton.addEventListener("click", () => showModal());
 
+// When the close button is clicked, hide the modal
 let closeButton = document.querySelector(".close-button")
 closeButton.addEventListener("click", () => hideModal());
 
@@ -14,4 +15,15 @@ window.addEventListener("click", event => {
   if (event.target == modal) {
     hideModal();
   }
+});
+
+let hiddenInputs = document.querySelectorAll(".form-container input.hidden");
+let loginButton = document.querySelector(".form-container button");
+
+let registerButton = document.querySelector(".register-button");
+registerButton.addEventListener("click", () => {
+  // When "Don't have an account?" is clicked, remove hidden class and set
+  // "Log in" button to say "Register" instead.
+  hiddenInputs.forEach(input => input.classList.remove("hidden"));
+  loginButton.innerHTML = "Register";
 });
